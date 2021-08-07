eric1234/psn-browse

commit a093b9352cc67ee2cfab7ba50b1fb91cf25ac716

Fix pushState
I had attempted to not make unnecessary changes to the querystring when
the state does not logically change. But then improving the URL to
redirect to I broke that.

This restores the proper handling of pushState.

source/frontend/search_state.js
@@ -15,7 +15,7 @@ export default class {
   constructor(facets, minYear) {
     this.facets = facets
 
-    const current = parse(this.currentQueryString().substr(1))
+    const current = parse(this.currentQueryString())
 
     // We have general defaults but IF the initial querystring has a search
     // query then adjust those defaults to use the min year
@@ -51,7 +51,9 @@ export default class {
 
   // A normalized version of the current querystring
   currentQueryString() {
-    return `?${stringify(parse(window.location.search.substr(1)))}`
+    let qs = `?${stringify(parse(window.location.search.substr(1)))}`
+    if( qs == '?' ) qs = ''
+    return qs
   }
 
   // What the querystring *should* be based on the state

commit 13c64f02a361d17d994ff15f12efc4cc839eabbc

Fix link to search
When the querystring is parsed it cannot include the `?`. This broke in
a093b93. I'm not sure why I dropped the `substr(1)` but it is needed.
Without it the `?` becomes part of a param name.

source/frontend/search_state.js
@@ -15,7 +15,7 @@ export default class {
   constructor(facets, minYear) {
     this.facets = facets
 
-    const current = parse(this.currentQueryString())
+    const current = parse(this.currentQueryString().substr(1))
 
     // We have general defaults but IF the initial querystring has a search
     // query then adjust those defaults to use the min year